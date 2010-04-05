179a5c0e61814f4fdf7e3fe4d19301b664f9c0aa
FATECITU - Faculdade de Tecnologia de Itu <ricardo.leme@fatecitu.edu.br>
2010-04-05 15:47:45 -0300

    frontend

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -1,5 +1,13 @@
 class PrincipalController < ApplicationController
   def index
-     @noticia = Noticia.find(:all)
-  end
+     @noticias = Noticia.find(:all)
+	 @eventos = Evento.find(:all)
+  end  
+  def procurar
+      @termo = params[:busca].downcase
+	  @noticias = Noticia.find(:all, :conditions => ["LOWER(titulo) LIKE ? or LOWER(textocurto) LIKE ? or LOWER(textolongo) LIKE ?", '%'+@termo+'%','%'+@termo+'%','%'+@termo+'%'])
+   unless @noticias.size > 0
+     flash.now[:info] = "Nao foi encontrada nenhuma noticia com esse criterio de busca!"
+   end	 
+  end 
 end
--- app/models/categorianoticia.rb
+++ app/models/categorianoticia.rb
@@ -2,4 +2,6 @@ class Categorianoticia < ActiveRecord::Base
  # Validacoes
   validates_presence_of   :descricao
   validates_uniqueness_of :descricao
+ #Relacionamentos
+  has_many :noticias 
 end
